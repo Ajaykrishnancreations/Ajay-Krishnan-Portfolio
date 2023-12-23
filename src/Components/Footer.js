c43eeab2ad6ab943e80086b202fb5c276f9052a7
Ajay krishnan creations <118874449+AjayKrishnanKen42@users.noreply.github.com>
2023-12-23 19:46:02 +0530

    screen added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,15 +21,15 @@ class Footer extends Component {
           <Fade bottom>
             <div className="twelve columns">
               <ul className="social-links">{networks}</ul>
-
+              <a href="/NewYear">
               <ul className="copyright">
                 <li>&copy; Copyright 2021 Ajay Krishnan Creations</li>
                 <li>
                 </li>
               </ul>
+              </a>
             </div>
           </Fade>
-
           <div id="go-top">
             <a className="smoothscroll" title="Back to Top" href="#home">
               <i className="icon-up-open"></i>
