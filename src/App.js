8f2574dc5e99169dc611d7bc3a3823e194fd6ee3
Ajay krishnan creations <118874449+AjayKrishnanKen42@users.noreply.github.com>
2024-03-04 12:48:37 +0530

    creator flow ui and api added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router,
+  //  Routes,
+  //   Route
+   } from "react-router-dom";
 import ReactGA from "react-ga";
 import $ from "jquery";
 import "./App.css";
-// import Header from "./Components/Header";
-// import Footer from "./Components/Footer";
-// import About from "./Components/About";
-// import Resume from "./Components/Resume";
-// import Contact from "./Components/Contact";
-// import Portfolio from "./Components/Portfolio";
-import NewYear from '../src/NewYear/NewYear';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Portfolio from "./Components/Portfolio";
+// import NewYear from '../src/NewYear/NewYear';
 
 class App extends Component {
   constructor(props) {
@@ -46,9 +49,17 @@ class App extends Component {
     return (
       <>
         <Router>
-          <Routes>
-            <Route path="/" element={<NewYear />} />
-          </Routes>
+          {/* <Routes> */}
+            {/* <Route path="/" element={<NewYear />} /> */}
+            <div className="App">
+            <Header data={this.state.resumeData.main} />
+            <About data={this.state.resumeData.main} />
+            <Resume data={this.state.resumeData.resume} />
+            <Portfolio data={this.state.resumeData.portfolio} />
+            <Contact data={this.state.resumeData.main} />
+            <Footer data={this.state.resumeData.main} />
+          </div>
+          {/* </Routes> */}
         </Router>
         {/* {window.location.pathname !== "/NewYear" && (
           <div className="App">
